a99defe68cc07400f9e4a754f49c947ca7cfd700
min <msb8080@163.com>
2018-04-19 22:08:05 +0800

    webgl编程指南的随课代码

---
+++ WEBGL/webgl练习/HelloPoint1.js
@@ -0,0 +1,14 @@
+//HelloPoint.js
+
+
+function main(){
+	var canvas = document.getElementById('webgl');
+	var gl = getWebGLContext(canvas);
+	if(!gl){
+		console.log('failed');
+		return;
+	}	
+	//设置背景色
+	gl.clearColor(0.0,0.0,1.0,1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+}
